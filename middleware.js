6e63131e86832772461a1a5667e699ec50c1b262
nora <163450896+24c02@users.noreply.github.com>
2026-02-11 13:45:30 -0500

    slash j

--- middleware.js
+++ middleware.js
@@ -1,4 +1,8 @@
 export default function middleware(req) {
+  const url = new URL(req.url);
+  if (url.pathname === "/") {
+    return Response.redirect("https://cdn.hackclub.com", 301);
+  }
   return Response.redirect(
     `https://cdn.hackclub.com/rescue?url=${encodeURIComponent(req.url)}`,
     301
